e0e1cd12f6708af0602f4b83071f7f9d8f13893f
lmsand <lmsand99@icloud.com>
2023-12-21 17:45:50 -0600

    Done checkmarks

--- ToDoList/ViewModels/ProfileViewViewModel.swift
+++ ToDoList/ViewModels/ProfileViewViewModel.swift
@@ -6,11 +6,23 @@
 //
 
 import Foundation
+import FirebaseAuth
+import FirebaseFirestore
 
 class ProfileViewViewModel: ObservableObject {
     init() {}
     
     func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone) // updating existing data
         
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
     }
 }
