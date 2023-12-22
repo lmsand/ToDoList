40b956e12f951c8753ea91b2eb1a333c72b03f48
lmsand <lmsand99@icloud.com>
2023-12-21 20:32:49 -0600

    Profile and Logout

--- ToDoList/ViewModels/ToDoListItemViewViewModel.swift
+++ ToDoList/ViewModels/ToDoListItemViewViewModel.swift
@@ -5,11 +5,25 @@
 //  Created by lexi sanders on 12/20/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 /// view model for single to do list item view (each row in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
     
-    
+    func toggleIsDone(item: ToDoListItem) {
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone) // updating existing data
+        
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
